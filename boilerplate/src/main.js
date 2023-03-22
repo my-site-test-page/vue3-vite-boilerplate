28a1c369aa257c2fd3d8d97084e636faefa6b129
kaba <sample>
2023-03-22 21:55:02 +0900

    append-vuetify

--- boilerplate/src/main.js
+++ boilerplate/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
 import './style.scss';
 import App from './App.vue';
+import vuetify from './vuetify';
 
 const app = createApp(App);
+
+const plugins = [vuetify];
+
+plugins.forEach((plugin) => app.use(plugin));
 
 app.mount('#app');
